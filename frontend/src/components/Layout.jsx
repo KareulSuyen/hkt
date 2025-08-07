b5d6097156ac6a1e966dce687eed3ede6a196a76
greysuyen <carlambay687@gmail.com>
2025-08-07 15:50:31 +0800

    style: sidebar default style

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -1,11 +1,15 @@
 import Navbar from '../components/Navbar';
 import { Outlet } from 'react-router-dom';
-
+import { useState } from 'react';
+import Sidebar from '../components/Sidebar';
 
 const Layout = () => {
-    return (
+    const [isSidebarOpen, setSidebarOpen] = useState(false);
+    const toggleSidebar = () => setSidebarOpen(prev => !prev);
+     return (
         <>
-            <Navbar />
+            <Navbar toggleSidebar={toggleSidebar}/>
+            <Sidebar isSidebarOpen={isSidebarOpen} toggleSidebar={toggleSidebar}/>
             <main>
                 <Outlet />
             </main>
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,21 @@
 import { NavLink } from 'react-router-dom';
+import navstyle from '../styles/navbar.module.scss';
 
-
-const Navbar = () => {
+const Navbar = ({toggleSidebar}) => {
+    const setActive = ({ isActive }) =>
+        isActive ? `${navstyle.link} ${navstyle.active}` : navstyle.link
     return (
         <>
-            <div>
-                <NavLink to='/'>Home</NavLink>
-                <NavLink to='/help'>Help</NavLink>
-            </div>
+            <nav className={navstyle['nav-bar']}>
+                <div className={navstyle['toggle-btn']}> {/* Navbar toggle btn */}
+                    <button onClick={toggleSidebar}>X</button>
+                </div>
+                <div className={navstyle['nav-center']}>
+                    <NavLink to='/' className={setActive}>Home</NavLink>
+                    <NavLink to='/help' className={setActive}>Help</NavLink>
+                    <NavLink className={setActive}>Profile</NavLink>
+                </div>
+            </nav>
         </>
     );
 };
