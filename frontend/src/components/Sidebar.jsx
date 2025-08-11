3d2a84962f032c4fe7a4497b763251059ce669a0
greysuyen <carlambay687@gmail.com>
2025-08-11 22:16:40 +0800

    feat: sidebar close icon

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,6 @@
 import { NavLink } from 'react-router-dom';
 import sidebarstyle from '../styles/sidebar.module.scss';
+import { TbLayoutSidebarRightExpandFilled } from "react-icons/tb";
 
 
 const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
@@ -9,7 +10,9 @@ const Sidebar = ({ isSidebarOpen, toggleSidebar }) => {
         <>
             <aside className={`${sidebarstyle.sidebar} ${isSidebarOpen ? sidebarstyle.open : sidebarstyle.closed}`}>
                 <div className={sidebarstyle['toggle-btn']}>
-                    <button onClick={toggleSidebar}>X</button>
+                    <button onClick={toggleSidebar}>
+                        <TbLayoutSidebarRightExpandFilled size={30} />
+                    </button>
                 </div>  
                 <div className={sidebarstyle['sidebar-links']}>
                     <NavLink to='#'>Test Link</NavLink>
