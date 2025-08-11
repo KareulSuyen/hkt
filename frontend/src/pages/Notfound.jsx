2b10de58476a5e9b11b43a29b0ebb5de1d02b856
greysuyen <carlambay687@gmail.com>
2025-08-11 12:28:59 +0800

    feat: icon adjustments

--- frontend/src/pages/Notfound.jsx
+++ frontend/src/pages/Notfound.jsx
@@ -1,4 +1,7 @@
 import styles from '../styles/notfound.module.scss';
+import { RiAliensLine } from "react-icons/ri";
+import { HiOutlineRocketLaunch } from "react-icons/hi2";
+
 
 const NotFound = () => {
     return (
@@ -9,8 +12,8 @@ const NotFound = () => {
                 <p className={styles.text}>The page you're looking for has gone on a little adventure.</p>
                 <div className={styles.illustration}>
                     <div className={styles.planet}></div>
-                    <div className={styles.rocket}>🚀</div>
-                    <div className={styles.alien}>👽</div>
+                    <div className={styles.rocket}><HiOutlineRocketLaunch size={55}/></div>
+                    <div className={styles.alien}><RiAliensLine size={45}/></div>
                 </div>
                 <a href="/" className={styles.button}>Take me home</a>
             </div>
