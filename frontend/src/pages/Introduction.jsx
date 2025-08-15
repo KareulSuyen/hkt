1caf16efbeceab109587d58ae595f39a41a573f6
greysuyen <carlambay687@gmail.com>
2025-08-15 13:49:00 +0800

    feat: word adjustments

--- frontend/src/pages/Introduction.jsx
+++ frontend/src/pages/Introduction.jsx
@@ -32,7 +32,7 @@ const Intro = () => {
           <div className={styles.heroText}>
             <h2>Pag-unawa sa Ating <span>Planetaryong Limitasyon</span></h2>
             <p>
-              Sa site na to, matututo ka tungkol sa overpopulation at mga hamon nito.
+              Sa site na to, matututo ka tungkol sa sobrang populasyon at mga hamon nito.
               Tuklasin ang kritikal na ugnayan ng paglaki ng populasyon, 
               pagkonsumo ng mga likas na yaman, at pagpapanatili ng kalikasan 
               sa ating panahon ng data-driven na mundo. Maaari ka ring sumubok
@@ -41,11 +41,11 @@ const Intro = () => {
             <div className={styles.heroStats}>
               <div className={styles.stat} id={styles.world}>
                 <div className={styles.statNumber}>8.1B</div>
-                <div className={styles.statLabel}>Kasalukuyang Populasyon</div>
+                <div className={styles.statLabel}>Kasalukuyang Populasyon ng mundo</div>
               </div>
               <div className={styles.stat} id={styles.stats}>
                 <div className={styles.statNumber}>112.7M</div>
-                <div className={styles.statLabel}>Populasyon sa Pilipinas</div>
+                <div className={styles.statLabel}>Kasalukuyang populasyon sa Pilipinas</div>
               </div>
             </div>
           </div>
