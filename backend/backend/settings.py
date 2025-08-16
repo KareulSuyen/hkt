fba3081eab1835d87f94473caf76dbbf71f84d8e
greysuyen <carlambay687@gmail.com>
2025-08-16 21:52:25 +0800

    feat: database config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -93,11 +93,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgressql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PWD'),
+        'HOST': os.getenv('DV_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
-
+ 
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
