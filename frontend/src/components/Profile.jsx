712d14276da5eb68add6f49d59b16ed1603aaa2b
greysuyen <carlambay687@gmail.com>
2025-08-18 11:41:55 +0800

    fix: Profile bar state

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,21 +1,36 @@
-import { NavLink } from 'react';
 import profilestyle from '../styles/profile.module.scss';
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
+import { useNavigate } from 'react-router-dom';
+import { CiLogout } from "react-icons/ci";
+import { RiUserSettingsLine } from "react-icons/ri";
 
 
-const Profile = ({ isProfileOpen, toggleProfile }) => {
-    <>
-        <aside className={`${profilestyle.profile} ${isProfileOpen ? profilestyle.open : profilestyle.closed}`}>
-            <div className={profilestyle['toggle-btn']}> {/* Profile toggle-btn */}
-                <button onClick={toggleProfile}>X</button>
+const Profile = ({  isProfileOpen, toggleProfile }) => {
+    const navigate = useNavigate();
+        const handleLogout = () => {
+            localStorage.removeItem(ACCESS_TOKEN);
+            localStorage.removeItem(REFRESH_TOKEN);
+            navigate('/logout');
+        };
+    return (
+        <>
+            <div className={`${profilestyle.profile} ${isProfileOpen ? profilestyle.open : profilestyle.closed}`}>
+                <div className={profilestyle['toggle-btn']}>
+                    <button onClick={toggleProfile}>X</button>
+                </div>
+                <div className={profilestyle['tab-contents']}>
+                    <button className={profilestyle['section-btn']}>
+                        <RiUserSettingsLine size={23} className={profilestyle['section-icon']} />
+                        Settings
+                    </button>
+                    <button onClick={handleLogout} className={profilestyle['section-btn']}>
+                        <CiLogout size={23} className={profilestyle['section-icon']} />
+                        Log Out
+                    </button>
+                </div>
             </div>
-            <div className={profilestyle['profile-links']}>
-                <p>Test</p>
-                <p>Test</p>
-                <p>Test</p>
-            </div>
-        </aside>
-    </>
-}
-
+        </>
+    );
+};
 
 export default Profile;
