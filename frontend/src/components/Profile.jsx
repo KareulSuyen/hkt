77e55145791ec19b4c33fb9f9d7a24a742aac93e
greysuyen <carlambay687@gmail.com>
2025-08-19 10:40:51 +0800

    feat: Logout logic

--- frontend/src/components/Profile.jsx
+++ frontend/src/components/Profile.jsx
@@ -1,9 +1,17 @@
-import { NavLink } from 'react';
+import { useNavigate } from 'react-router-dom';
 import profilestyle from '../styles/profile.module.scss';
 import { TbLayoutSidebarRightCollapseFilled } from "react-icons/tb";
-
+import { ACCESS_TOKEN, REFRESH_TOKEN } from '../constants';
 
 const Profile = ({ isProfileOpen, toggleProfile }) => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        localStorage.removeItem(ACCESS_TOKEN);
+        localStorage.removeItem(REFRESH_TOKEN);
+        navigate('/introduction'); 
+    };
+
     return (
         <>
             <aside className={`${profilestyle.profile} ${isProfileOpen ? profilestyle.open : profilestyle.closed}`}>
@@ -15,16 +23,15 @@ const Profile = ({ isProfileOpen, toggleProfile }) => {
                 <div className={profilestyle['profile-links']}>
                     <a href="#">Test</a>
                     <a href="#">Test</a>
-                    <a href="#">Test</a>
 
                     <div className={profilestyle['logout-btn']}>
-                        <button>Log Out</button>
+                        <button onClick={handleLogout}>Log Out</button>
                     </div>
                 </div>
             </aside>
         </>
-    )
-}
+    );
+};
 
 
 export default Profile;
