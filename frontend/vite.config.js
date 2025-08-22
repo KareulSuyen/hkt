517d9a31e4c96969bffab866abbce859c01e3312
greysuyen <carlambay687@gmail.com>
2025-08-22 17:51:56 +0800

    feat: intro-logo media queries

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './',  // This fixes the asset paths
+  base: './',  
   build: {
     outDir: 'dist',
   },
