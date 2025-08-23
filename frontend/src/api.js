192d220d1c28e7d216f487cae7d7134416207dd4
greysuyen <carlambay687@gmail.com>
2025-08-23 10:23:13 +0800

    feat: email verification

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -120,4 +120,27 @@ export const testAPIConnection = async () => {
   }
 };
 
+export const verifyEmail = async (token) => {
+  try {
+    const response = await api.post('/user/verify-email/', {
+      token: token
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+export const resendVerification = async (email) => {
+  try {
+    const response = await api.post('/user/resend-verification/', {
+      email: email
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
+
 export default api;
